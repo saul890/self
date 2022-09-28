88b9e79742944845cb641b3d5d76de3ea8c32a81
Saul Hirner <saulhirner@gmail.com>
2022-09-28 19:13:34 -0400

    continue

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const imageOne = document.getElementById('imageOne');
 const imageTwo = document.getElementById('imageTwo');
 const imageThree = document.getElementById('imageThree');
+const imageFour = document.getElementById('imageFour');
+const imageFive = document.getElementById('imageFive');
 
 imageOne.addEventListener('click', function onClick(event) {
   imageTwo.style.display = 'block';
   imageThree.style.display = 'block';
+  imageFour.style.display = 'block';
+});
+
+imageFour.addEventListener('click', function onClick(event) {
+  imageFive.style.display = 'block';
 });
