f5796adc34161194b2926c0fc7b57ffe26dda688
Saul Hirner <saulhirner@gmail.com>
2022-09-29 20:28:19 -0400

    bus commit

--- script.js
+++ script.js
@@ -3,13 +3,158 @@ const imageTwo = document.getElementById('imageTwo');
 const imageThree = document.getElementById('imageThree');
 const imageFour = document.getElementById('imageFour');
 const imageFive = document.getElementById('imageFive');
+const imageSix = document.getElementById('imageSix');
+const imageSeven = document.getElementById('imageSeven');
+const imageEight = document.getElementById('imageEight');
+const imageNine = document.getElementById('imageNine');
+const imageTen = document.getElementById('imageTen');
+const image6 = document.getElementById('image6');
+const image7 = document.getElementById('image7');
+const image8 = document.getElementById('image8');
+const image9 = document.getElementById('image9');
+const image10 = document.getElementById('image10');
+const image11 = document.getElementById('image11');
+const image12 = document.getElementById('image12');
+const image13 = document.getElementById('image13');
+const image14 = document.getElementById('image14');
+const image15 = document.getElementById('image15');
+const image16 = document.getElementById('image16');
+const image17 = document.getElementById('image17');
+const image18 = document.getElementById('image18');
+const image19 = document.getElementById('image19');
+const image20 = document.getElementById('image20');
+const image21 = document.getElementById('image21');
+const image22 = document.getElementById('image22');
+const image23 = document.getElementById('image23');
+const image24 = document.getElementById('image24');
+const image25 = document.getElementById('image25');
+const image26 = document.getElementById('image26');
+const image27 = document.getElementById('image27');
+const image28 = document.getElementById('image28');
+const image29 = document.getElementById('image29');
+const image30 = document.getElementById('image30');
+const image31 = document.getElementById('image31');
+const image32 = document.getElementById('image32');
+const image33 = document.getElementById('image33');
+const image34 = document.getElementById('image34');
+const image35 = document.getElementById('image35');
+const image36 = document.getElementById('image36');
+const image37 = document.getElementById('image37');
+const image38 = document.getElementById('image38');
+const image39 = document.getElementById('image39');
+const image40 = document.getElementById('image40');
+const image41 = document.getElementById('image41');
+const image42 = document.getElementById('image42');
+const image43 = document.getElementById('image43');
+const image44 = document.getElementById('image44');
+const image45 = document.getElementById('image45');
+
+
 
 imageOne.addEventListener('click', function onClick(event) {
   imageTwo.style.display = 'block';
-  imageThree.style.display = 'block';
-  imageFour.style.display = 'block';
 });
+
+imageTwo.addEventListener('click', function onClick(event) {
+  imageThree.style.display = 'block';
+});
+
+imageThree.addEventListener('click', function onClick(event) {
+  imageFour.style.display = 'block';
+  imageTwo.style.top = '100px';
+  imageThree.style.top = '200px';
+});
+
 
 imageFour.addEventListener('click', function onClick(event) {
   imageFive.style.display = 'block';
+  image6.style.display = 'block';
+  image7.style.display = 'block';
+  image8.style.display = 'block';
+  image9.style.display = 'block';
+  image10.style.display = 'block';
+  image11.style.display = 'block';
+  image12.style.display = 'block';
+  image13.style.display = 'block';
+  image14.style.display = 'block';
+  image15.style.display = 'block';
+  image16.style.display = 'block';
+  image17.style.display = 'block';
+  image18.style.display = 'block';
+  image19.style.display = 'block';
+  image20.style.display = 'block';
+  image21.style.display = 'block';
+  image22.style.display = 'block';
+  image23.style.display = 'block';
+  image24.style.display = 'block';
+  image25.style.display = 'block';
+  image26.style.display = 'block';
+  image27.style.display = 'block';
+  image28.style.display = 'block';
+  image29.style.display = 'block';
+  image30.style.display = 'block';
+  image31.style.display = 'block';
+  image32.style.display = 'block';
+  image33.style.display = 'block';
+  image34.style.display = 'block';
+  image35.style.display = 'block';
+  image36.style.display = 'block';
+  image37.style.display = 'block';
+  image38.style.display = 'block';
+  image39.style.display = 'block';
 });
+
+image8.addEventListener('click', function onClick(event) {
+  image40.style.display = 'block';
+});
+image17.addEventListener('click', function onClick(event) {
+  image40.style.display = 'block';
+});
+image21.addEventListener('click', function onClick(event) {
+  image40.style.display = 'block';
+});
+image30.addEventListener('click', function onClick(event) {
+  image40.style.display = 'block';
+});
+image39.addEventListener('click', function onClick(event) {
+  image40.style.display = 'block';
+});
+image40.addEventListener('click', function onClick(event) {
+  image41.style.display = 'block';
+});
+
+image41.addEventListener('click', function onClick(event) {
+  image42.style.display = 'block';
+});
+
+image42.addEventListener('click', function onClick(event) {
+  image43.style.display = 'block';
+});
+
+image43.addEventListener('click', function onClick(event) {
+  image44.style.display = 'block';
+});
+
+image44.addEventListener('click', function onClick(event) {
+  image45.style.display = 'block';
+});
+
+
+
+
+
+
+
+
+
+imageFive.addEventListener('click', function onClick(event) {
+  imageSix.style.display = 'block';
+});
+
+imageSix.addEventListener('click', function onClick(event) {
+  imageSeven.style.display = 'block';
+  imageEight.style.display = 'block';
+  imageNine.style.display = 'block';
+  imageTen.style.display = 'block';
+});
+
